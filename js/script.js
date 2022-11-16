081309f9a23338738d2ba932278d68ba45281183
Aleksandra <haruka.lex@gmail.com>
2022-11-16 20:41:17 +0100

    code syntax improved

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-
+{
 
 function playGame(playerInput){
     
@@ -15,32 +15,32 @@ function playGame(playerInput){
     };
     
     // let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
-    let playerMove = getMoveName(playerInput);
-    let randomNumber = Math.floor(Math.random() * 3 + 1);
-    let computerMove = getMoveName(randomNumber);
+    const playerMove = getMoveName(playerInput),
+        randomNumber = Math.floor(Math.random() * 3 + 1),
+        computerMove = getMoveName(randomNumber);
     
     printMessage('Twój ruch to: ' + playerMove);
     printMessage('Komputer wybrał: ' + computerMove);
     
-    function displayResult(argPlayerMove,argComputerMove){
-        if ( playerMove == "kamień" && argComputerMove == "papier") {
-            printMessage('Przegrałeś!');
-        }else if (argPlayerMove == "kamień" && argComputerMove == "nożyce") {
-            printMessage('Ty wygrywasz!');
-        }else if (argPlayerMove == "papier" && argComputerMove == "kamień") {
-            printMessage('Ty wygrywasz!');
-        }else if (argPlayerMove == "papier" && argComputerMove == "nożyce") {
-            printMessage('Przegrałeś');
-        }else if (argPlayerMove == "nożyce" && argComputerMove == "papier") {
-            printMessage('Ty wygrywasz!');
-        }else if (argPlayerMove == "nożyce" && computerMove == "kamień") {
-            printMessage('Przegrałeś');
-        }else if (argPlayerMove>3 || playerInput<0) {
-            printMessage('Błąd! wybierz cyfrę od 1 do 3!')
-        }else{
-            printMessage('Remis');
+    const displayResult = function (argPlayerMove,argComputerMove){
+            if ( playerMove == "kamień" && argComputerMove == "papier") {
+                printMessage('Przegrałeś!');
+            }else if (argPlayerMove == "kamień" && argComputerMove == "nożyce") {
+                printMessage('Ty wygrywasz!');
+            }else if (argPlayerMove == "papier" && argComputerMove == "kamień") {
+                printMessage('Ty wygrywasz!');
+            }else if (argPlayerMove == "papier" && argComputerMove == "nożyce") {
+                printMessage('Przegrałeś');
+            }else if (argPlayerMove == "nożyce" && argComputerMove == "papier") {
+                printMessage('Ty wygrywasz!');
+            }else if (argPlayerMove == "nożyce" && argComputerMove == "kamień") {
+                printMessage('Przegrałeś');
+            }else if (argPlayerMove>3 || playerInput<0) {
+                printMessage('Błąd! wybierz cyfrę od 1 do 3!')
+            }else{
+                printMessage('Remis');
+            };
         };
-    };
     
     displayResult(playerMove,computerMove);
 };
@@ -57,3 +57,4 @@ document.getElementById('play-scissors').addEventListener('click', function(){
     playGame('3');
 });
 
+}
